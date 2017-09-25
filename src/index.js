a92eb377a08a4f7f50d2a2b7bc6899ec0c2d6db7
Ross Petrenko <petrenko.rostislav@gmail.com>
2017-09-25 19:34:19 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function multiply(first, second) {
-  // your solution
+
+	first = first.split('').reverse();
+	second = second.split('').reverse();
+	var result = [];
+
+	for(var i = 0; first[i] >= 0; i++){
+		for(var j = 0 ; second[j] >= 0; j++){
+			if(!result[i+j]){
+				result[i+j] = 0;
+			}
+			result[i+j]+=first[i]*second[j];
+		}
+	}
+	for(var i = 0;result[i]>=0;i++){
+		if(result[i]>=10){
+			if(!result[i+1]){
+				result[i+1] = 0;
+		}
+		result[i+1] +=parseInt(result[i]/10);
+		result[i]%=10;
+		}
+	}
+return result.reverse().join('');
 }
